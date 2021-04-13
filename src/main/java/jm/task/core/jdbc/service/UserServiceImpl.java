9f374e37890fe370af9a4c87d56949e735573167
volginys <volginys12@mail.ru>
2021-04-13 21:23:09 +0300

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserServiceImpl extends UserDaoHibernateImpl implements UserService {
+
+
+    public void createUsersTable() {
+        super.createUsersTable();
+    }
+
+    public void dropUsersTable() {
+        super.dropUsersTable();
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        super.saveUser(name, lastName, age);
+    }
+
+    public void removeUserById(long id) {
+        super.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return super.getAllUsers();
+    }
+
+    public void cleanUsersTable() {
+        super.cleanUsersTable();
+    }
+    public void close(){
+        super.close();
+    }
+}
